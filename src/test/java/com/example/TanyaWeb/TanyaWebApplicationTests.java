57af25b1e23aa57bcda53ccb8f45a63cbea0916f
Tanv7 <tanv7@yandex.ru>
2024-04-24 17:17:06 +0300

    Initial commit

---
+++ src/test/java/com/example/TanyaWeb/TanyaWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TanyaWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TanyaWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
